63ef6234591b47fd9db5d7768bf40d0bec466eed
harumhl <haru.mhl@gmail.com>
2016-05-22 00:11:11 -0500

    Update main.js

--- main.js
+++ main.js
@@ -5,3 +5,12 @@ game.state.add('jumbodrum', jumbodrumState);
 
 game.state.start('jumbodrum'); // SHOULD BE 'SELECTION'
 
+window.onload = function {
+  var host = location.origin.replace(/^http/, 'ws')
+  var ws = new WebSocket(host);
+  ws.onmessage = function (event) {
+    var li = document.createElement('li');
+    li.innerHTML = JSON.parse(event.data);
+    document.querySelector('#pings').appendChild(li);
+};
+}

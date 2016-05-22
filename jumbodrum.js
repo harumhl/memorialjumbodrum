c249f3872441d33470f4b85972020165bcb5ee31
harumhl <haru.mhl@gmail.com>
2016-05-21 19:00:52 -0500

    adding states

--- jumbodrum.js
+++ jumbodrum.js
@@ -1,1 +1,4 @@
 var game = new Phaser.Game(1000, 1000+129, Phaser.AUTO, 'MemorialJumboDrum'/*, { preload: preload, create: create, update: update }*/);
+
+game.state.add('selection', selectionState);
+game.state.add('jumbodrum', jumbodrumState);
